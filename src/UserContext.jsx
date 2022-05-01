d255d9e6d7f354660093831a2893e6511da8ea71
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-30 22:36:05 -0300

    validação do login, login automatico e redirecionamento quando o usuario ja estiver logado

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,6 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { TOKEN_POST, USER_GET } from './api';
+import React, { createContext, useCallback, useEffect, useState } from 'react'
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = createContext();
 
@@ -9,10 +10,20 @@ export const UserStorage = ({ children }) => {
    const [login, setLogin] = useState(null);
    const [loading, setLoading] = useState(false);
    const [error, setError] = useState(null);
+   const navigate = useNavigate();
 
-   useEffect(() => {
+   const userLogout = useCallback(
+      async function () {
+         setData(null);
+         setError(null);
+         setLoading(false);
+         setLogin(false);
+         window.localStorage.removeItem('token');
+         navigate('/login');
+      }, [navigate]
+   )
 
-   }, []);
+
 
    async function getUser(token) {
       const { url, options } = USER_GET(token);
@@ -23,15 +34,47 @@ export const UserStorage = ({ children }) => {
    }
 
    async function userLogin(username, password) {
-      const { url, options } = TOKEN_POST({ username, password });
-      const tokenRes = await fetch(url, options);
-      const { token } = await tokenRes.json();
-      window.localStorage.setItem('token', token);
-      getUser(token);
+      try {
+         setError(null);
+         setLoading(true);
+         const { url, options } = TOKEN_POST({ username, password });
+         const tokenRes = await fetch(url, options);
+         if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.status}`)
+         const { token } = await tokenRes.json();
+         window.localStorage.setItem('token', token);
+         await getUser(token);
+         navigate('/conta');
+      } catch (err) {
+         setError(err.message);
+         setLogin(false);
+      } finally {
+         setLoading(false);
+      }
    }
 
+   useEffect(() => {
+      async function autoLogin() {
+         const token = window.localStorage.getItem('token');
+         if (token) {
+            try {
+               setError(null);
+               setLoading(true);
+               const { url, options } = TOKEN_VALIDATE_POST(token);
+               const response = await fetch(url, options);
+               if (!response.ok) throw new Error('Token inválido');
+               await getUser(token);
+            } catch (err) {
+               userLogout();
+            } finally {
+               setLoading(false);
+            }
+         }
+      }
+      autoLogin();
+   }, [userLogout]);
+
    return (
-      <UserContext.Provider value={{ userLogin, data }}>
+      <UserContext.Provider value={{ userLogin, data, userLogout, error, loading, login }}>
          {children}
       </UserContext.Provider>
    )
