cdd33755a576ee0b8ac687e504a746a680aaff7b
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-30 21:38:16 -0300

    adionado contexto, custom hook e nome de usuario condicionado a estar logado ou não, com localstorage

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,34 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
-   const username = useForm('email');
+   const username = useForm();
    const password = useForm();
 
-   const handleSubmit = (event) => {
+   const { userLogin } = useContext(UserContext);
+
+   async function handleSubmit(event) {
       event.preventDefault();
 
       if (username.validate() && password.validate()) {
-         fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-               'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(),
-         })
-            .then(response => {
-               console.log(response);
-               return response.json();
-            })
-            .then(json => {
-               console.log(json)
-            })
+         userLogin(username.value, password.value)
       }
    }
-
 
    return (
       <div>
